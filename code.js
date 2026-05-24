8abe121670b64e04b0b38058e720dc668e4f7194
oakesoe110-hash <oakesoe110@gmail.com>
2026-05-25 01:52:10 +0630

    second code

---
+++ code.js
@@ -0,0 +1,132 @@
+/*
+var head = document.querySelector(".head");
+head.style.color = "red";
+head.style.fontSize ="30px";
+head.style.fontFamily = "Arial";
+head.style.textAlign = "center";
+head.style.textShadow = "2px 2px 4px #000000";
+*/
+/*let name;
+let age=22;
+name = age > 20 ? 'Oake' : 'khat';
+console.log(name); */
+
+/*let color = "blue";
+
+
+switch (color) {
+    case "blue" : console.log("The color is blue");break;
+    case "red" : console.log ("The color is red");break;
+    case "green" : console.log ("The color is green");break;
+    default : console.log ("The color is not blue, red, or green");
+} */
+
+// for loop
+/*for (let b = 0; b < 5; b++) {
+    console.log("Hellow World"+b);
+    }
+
+//while loop
+let j = 0;
+while (j < 5) {
+    console.log ("My Number is" + j);
+    j++;
+}
+
+
+ let data = ["oake", "khat", "Soe", "lwin"];
+
+ // for loop
+ for(let i=0; i< data.length; i++){
+    console.log(data[i]);
+ }
+
+ // foreach loop
+ data.forEach((val , b) => {
+    console.log(val + " " + b);
+ });
+
+ // while loop
+ let k=0;
+ while (k<data.length){
+    console.log(data[k]);
+    k++;
+ }
+
+ // do while loop
+ let m=0;
+ do{
+    console.log(data[m]);
+    m++;
+ } while (m<data.length);
+
+
+// for in 
+for (a in data){
+    console.log(a);w3e2
+}*/
+
+/* let num = "24Hello";
+let result = Number(num);
+if (isNaN(result)) {
+   console.log("The result is not a number");
+} else {
+   console.log("The result is a number: " + result);
+}
+
+function add(a,b){
+   y
+} */
+/* function add(num1, num2, num3, num4){
+   let sum= num1 + num2 + num3 + num4;
+   return sum;
+}
+let result = add(1,2,4,1);
+console.log(result); */
+
+// Call back function
+/* let expression = function (num, fun){
+   let result = fun(num);
+   return console.log(result*2);
+}
+expression(5, function(num_1){
+   return num_1 + 2;
+}) */
+
+
+// Expression function and arrow function
+/* let expression = (one, two ) => {
+   let sum = one + two;
+   return sum;
+}
+sum = expression(3, 4);
+console.log(sum); */
+
+/* let example = (three, four) => console.log(three * four);
+
+example(5, 6); */
+
+
+// map method
+/* names = ["oake", "khat", "soe", "lwin"];
+let newNames = names.map((name) => {
+   return "mr. " + name;
+})
+console.log(newNames); */
+
+// filter method
+/* let numbers = [1, 2, 3, 4, 5, 6];
+let evenNumbers = numbers.filter((num) => {
+   return num % 2 === 0;
+})
+console.log(evenNumbers);
+
+
+for(i in numbers){
+   if (numbers[i] % 2 ===0){
+      console.log(numbers[i]);
+   } else {
+      console.log("This is odd number");
+   }
+} */
+
